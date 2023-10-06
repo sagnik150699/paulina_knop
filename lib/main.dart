b91590269fa776617cf00035bc883a231ce01c8d
sagnik <sagnikbhattacharya1506@gmail.com>
2023-10-06 22:41:41 +0530

    GitHub workflow Test.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setPathUrlStrategy();
+
   if (kIsWeb) {
     await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform
